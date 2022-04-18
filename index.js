c6c70b6a28ac998b2c625f6e6835eb4fe64097af
Bhanunjay Kumar <bindasho@yahoo.com>
2022-04-18 18:10:42 +0530

    test

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+//const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+  'transports': ['websocket']
+});
 const port = process.env.PORT || 8081;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-io.set('transports', ['websocket']);
+
+//io.set('transports', ['websocket']);
+
+
 io.on('connection', (socket) => {
 	console.log("connection made");
   socket.on('chat message', msg => {
