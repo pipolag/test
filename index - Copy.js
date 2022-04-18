f989c635b8e0fcf3005592c01558cbca0631b215
Bhanunjay Kumar <bindasho@yahoo.com>
2022-04-18 11:50:05 +0530

    test

--- index - Copy.js
+++ index - Copy.js
@@ -1,8 +1,6 @@
 const app = require('express')();
-//const http = require('http').Server(app);
-const https = require('https').Server(app);;
-//const io = require('socket.io')(http);
-const io = require('socket.io')(https);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 8081;
 
 app.get('/', (req, res) => {
@@ -14,11 +12,7 @@ io.on('connection', (socket) => {
     io.emit('chat message', msg);
   });
 });
-/*
+
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
-});*/
-
-https.listen(port, () => {
-  console.log(`Socket.IO server running at https://localhost:${port}/`);
 });
